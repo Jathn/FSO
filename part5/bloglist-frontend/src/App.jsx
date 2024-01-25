d07763626787f4d1700bff762bda7db410b36a61
Jathn <jonathan.ramstedt@gmail.com>
2024-01-25 09:37:43 +0200

    Began first implementations of blogs frontend part5

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,19 +1,81 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
+    blogService.getAll(user).then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+
   return (
     <div>
       <h2>blogs</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,25 @@
+const BlogForm = ({ createBlog }) => {
+    const [newBlog, setNewBlog] = useState('');
+
+    const handleBlogChange = (event) => {
+        setNewBlog(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        createBlog({
+            content: newBlog,
+            important: true,
+        });
+        setNewBlog('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={newBlog} onChange={handleBlogChange} />
+            <button type="submit">save</button>
+        </form>
+    );
+};
+
+export default BlogForm;
